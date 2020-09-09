0e701efe6694412b89f028e55406008484f2d92f
arshavir azarfar <altair.cena@gmail.com>
2020-09-09 13:36:23 +0430

    project 5 added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: '/portfolio',
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio`,
+    description: `Arshavir Azarfar`,
+    author: `@arshazar`,
   },
   plugins: [
     {
@@ -33,11 +34,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `iPortfolio`,
+        short_name: `portfo`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000`,
+        theme_color: `#000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,18 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-//     if (stage === "build-html" || stage === "develop-html") {
-//         actions.setWebpackConfig({
-//             module: {
-//                 rules: [
-//                     {
-//                         test: /locomotive-scroll/,
-//                         use: loaders.null(),
-//                     },
-//                 ],
-//             },
-//         })
-//     }
-// }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html" || stage === "develop-html") {
+        actions.setWebpackConfig({
+            module: {
+                rules: [
+                    {
+                        test: /locomotive-scroll/,
+                        use: loaders.null(),
+                    },
+                    {
+                        test: /three/,
+                        use: loaders.null(),
+                    }
+                ],
+            },
+        })
+    }
+}
